7f83cdbaba06362b16159bfcd37815448212875a
Jerome Sloss <jeromesloss@yahoo.com>
2023-06-15 08:48:55 -0400

    message

--- app.js
+++ app.js
@@ -42,144 +42,216 @@
 //     console.log(adventurer.belongings[i]);
 // }
 
-// create a property for an object that is an array
-
+// const movies = [
+//     {
+//         title: "Tokyo Story"
+//     },
+//     {
+//         title: "Paul Blart: Mall Cop"
+//     },
+//     {
+//         title: "L'Avventura"
+//     }
+// ];
+// console.log(movies[0].title) //object
+// // Looping over array of objects
+// for(let i = 0; i < movies.length; i++) {
+//     console.log(movies[i].title);
+// }
+// Combine objects, arrays, and functions
 // const foo = {
-//     someArray:[1, 2, 3]
-// };
-// foo.someArray[0];
-
-// create a property for an object that is an object
-
-// const foo = {
-//     someObject: {
-//         someProperty: 'oh hai!'
+//     arr: [1,2,3],
+//     obj: {
+//         prop: 'object property'
+//     },
+//     doSomthing: () => {
+//         console.log("I did something!")
 //     }
-// };
-// foo.someObject.someProperty;
-
-// You can create a property for an object that is a function (method)
-
-// const foo = {
-//     someMethod: ()=>{
-//         console.log('oh hai');
+// }
+// console.log(foo.arr[0]); //1
+// console.log(foo.obj.prop) //'object property'
+// foo.doSomthing();
+//AN array of arrays
+// const foo = [
+//     [1, 2, 3],
+//     ["4", "5", "6"],
+//     [7, 8, 9],
+//     ()=>{
+//         console.log("I am a function inside an array")
 //     }
-// };
-// foo.someMethod();
-
-// const player = {
-//     name: 'Josh the great',
-//     health: 1000,
-//     power: 1000,
-//     stamina: 1000
-// }
-
-// const bigBadBoss = {
-//     name: 'Magnardo the Merciless',
-//     health: 1000000,
-//     power: 1000000,
-//     stamina: Infinity
-// }
-// // What about function
-// const createEnemy = (nameIs, healthIs, powerIs, staminaIs) => {
-//     const newEnemy = {
-//         name: nameIs,
-//         health: healthIs,
-//         power: powerIs,
-//         stamina: staminaIs
-//     }
-//     return newEnemy
-// }
-
-// const createPlayer = (nameIs, healthIs, powerIs, staminaIs) => {
-//     const newPlayer = {
-//         name: nameIs,
-//         health: healthIs,
-//         power: powerIs,
-//         stamina: staminaIs
-//     }
-//     return newPlayer
-// }
-
-
-// // creating a class
-
+// ]
+// console.log(foo[0]) //[]
+// console.log(foo[0][1]) //2
+// //Storing functions in an array
+// console.log(foo[3]());
+//CREATING A CLASS
 // class Character {
-//     constructor (name, age, eyes, hair, lovesCats = false, lovesDogs) {
-//       this.legs = 2;
-//       this.arms = 2;
-//       this.name = name;
-//       this.age = age;
-//       this.eyes = eyes;
-//       this.hair = hair;
-//       this.lovesCats = lovesCats;
-//       this.lovesDogs = lovesDogs || false;
+//     constructor (name, age, eyes, hair, lovesCats = true, lovesDogs) {
+//         this.legs = 2;
+//         this.arms = 2;
+//         this.name = name;
+//         this.age = age;
+//         this.eyes = eyes,
+//         this.hair = hair;
+//         this.lovesCats = lovesCats;
+//         this.lovesDogs = lovesDogs || true;
 //     }
+//     setHairColor (HairColor) {
+//         this.hair = HairColor;
+//     }
+//     //method1
 //     greet (otherCharacter) {
-//       console.log('hi ' + otherCharacter + '!');
+//         console.log(`Hello ${otherCharacter}`);
 //     }
+//     //method2
 //     smite () {
-//       console.log('I smite thee you vile person');
+//         console.log('I smite thee you vile person!')
 //     }
+// }
+// const alexander = new Character();
+// const brock = new Character();
+// // alexander.greet("Brock");
+// // brock.greet("Alexander")
+// // alexander.smite()
+// //name. age, eyes, hair, cats, dogs
+// const azreal = new Character('azreal', '150', 'green', 'silver');
+// azreal.setHairColor('red')
+// console.log(azreal);
+
+class Character {
+  constructor(name, age, eyes, hair, lovesCats = true, lovesDogs) {
+    this.legs = 2;
+    this.arms = 2;
+    this.name = name;
+    this.age = age;
+    this.eyes = eyes;
+    this.hair = hair;
+    this.lovesCats = lovesCats;
+    this.lovesDogs = lovesDogs || true;
+  }
+  greet(otherCharacter) {
+    console.log("hi " + otherCharacter + "!");
+  }
+  classyGreeting(otherClassyCharacter) {
+    console.log("Howdy " + otherClassyCharacter.name + "!");
+  }
+  setHair(hairColor) {
+    this.hair = hairColor;
+  }
+  smite() {
+    console.log("i smited thee.");
+  }
+// class Character {
+//   constructor(name, age, eyes, hair, lovesCats = true, lovesDogs) {
+//     this.legs = 2;
+//     this.arms = 2;
+//     this.name = name;
+//     this.age = age;
+//     this.eyes = eyes;
+//     this.hair = hair;
+//     this.lovesCats = lovesCats;
+//     this.lovesDogs = lovesDogs || true;
 //   }
-//   const you = new Character('Cathy the Miraculous', 29, 'brown', 'locs of dark brown', true, true);
-//   const me = new Character('Arthur the Wavy', 32, 'brown', 'his waves be spinnin keep of the g for greatness');
-//   console.log(me);
+//   greet(otherCharacter) {
+//     console.log("hi " + otherCharacter + "!");
+//   }
+//   classyGreeting(otherClassyCharacter) {
+//     console.log("Howdy " + otherClassyCharacter.name + "!");
+//   }
+//   setHair(hairColor) {
+//     this.hair = hairColor;
+//   }
+//   smite() {
+//     console.log("i smited thee.");
+//   }
+// }
+// const hobbit = new Character("Mr Baggins", 33, "brown", "black");
+// // console.log(hobbit);
 
-// factory functions
-
-// class Car {
-//     constructor(make, vin) {
-//         this.make = make;
-//         this.vin = vin;
+// class Hobbit extends Character {
+//     constructor (name, age, eyes, hair) {
+//         super(name, age, eyes, hair);
+//         this.skils = ['thievery', 'speed', 'willpower']
 //     }
-//     // methods
-//     drive() {
-//         console.log('vroom vroom')
+//     steal () {
+//         console.log('lets get away!')
+//     }
+
+//     smite() {
+//         super.super();
+//         this.steal();
 //     }
 // }
 
-// create an object
+// const frodo = new Hobbit('frodo', 30, 'blue', 'black')
+// // console.log(frodo)
+// frodo.classyGreeting(hobbit);
+// frodo.smite()
 
-// const mazda = new Car('Mazda', 12345678);
+// FACOTRY FUNCTIONS
+
+class Car {
+    constructor(make, vin) {
+        this.make = make;
+        this.vin = vin;
+    }
+    //methods
+    drive() {
+        console.log('vroom vroom')
+    }
+}
+const hobbit = new Character("Mr Baggins", 33, "brown", "black");
+// console.log(hobbit);
+
+class Hobbit extends Character {
+    constructor (name, age, eyes, hair) {
+        super(name, age, eyes, hair);
+        this.skils = ['thievery', 'speed', 'willpower']
+//create an object
+
+const mazda = new Car('Mazda', 123345687586);
 // console.log(mazda)
 
-// class Factory {
-//     constructor (company) {
-//         this.company = company;
-//         this.cars.push(newCar)
-//     }
-// }
-
-// generateCar () {
-//     const newCar = new Car(this.company, this.cars.length); {
-//     this.cars.push(newCar);
-//   }
-//   findCar (index) {
-//     return this.cars[index];
-//   }
-// }
-// const tesla = new Factory('Tesla');
-// tesla.generateCar();
-// tesla.generateCar();
-// tesla.generateCar();
-// tesla.generateCar();
-// console.log(tesla);
-// console.log(tesla.findCar(0));
-
-// const porche = new Factory('Porche');
-// porche.generateCar();
-// porche.generateCar();
-// console.log(porche);
-// console.log(porche.findCar(0));
-
-// create static property for class
-
-class Person {
-    static eyeColors () {
-      return ['blue', 'green', 'brown'];
+class Factory {
+    constructor (company) {
+        this.company = company;
+        this.cars = [];
     }
-    // rest of class definition here...
-  }
-  // more code...
-  const superman = new SuperHero('Clark Kent', 30, Person.eyeColors()[0], 'black');
+    steal () {
+        console.log('lets get away!')
+
+    generateCar() {
+        const newCar = new Car(this.company, this.cars.length);
+        this.cars.push(newCar)
+    }
+
+    smite() {
+        super.super();
+        this.steal();
+    findCar (index) {
+        return this.cars[index]
+    }
+}
+
+const frodo = new Hobbit('frodo', 30, 'blue', 'black')
+// console.log(frodo)
+frodo.classyGreeting(hobbit);
+frodo.smite()
+const tesla = new Factory('Tesla');
+// console.log(tesla)
+tesla.generateCar();
+tesla.generateCar();
+tesla.generateCar();
+tesla.generateCar();
+// console.log(tesla)
+//find car
+// console.log(tesla.findCar(0))
+
+
+const porche = new Factory('Porche');
+porche.generateCar()
+porche.generateCar()
+porche.generateCar()
+porche.generateCar()
+
+console.log(porche);
